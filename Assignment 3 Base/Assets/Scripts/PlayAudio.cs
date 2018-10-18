0dae07c23496e7f74bd23cdc4db467217caf9c4b
Lachlan <n9745718@qut.edu.au>
2018-10-18 23:54:27 +1000

    TILES / LEVELS DONE

---
+++ Assignment 3 Base/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    public AudioClip audio1;
+    public AudioClip audio2;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<AudioSource>().clip = audio1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
